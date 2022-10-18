339cf1591c9c1df57862e367e52dc01d9eb62268
twozeronine <amdusias209@gmail.com>
2022-10-19 01:14:08 +0900

    feat : login auth

--- lib/exauth_web/controllers/auth_controller.ex
+++ lib/exauth_web/controllers/auth_controller.ex
@@ -1,6 +1,8 @@
 defmodule ExauthWeb.AuthController do
   use ExauthWeb, :controller
   alias Exauth.Accounts
+  alias Exauth.Accounts.User
+  alias ExauthWeb.JWTToken
 
   def ping(conn, _params) do
     conn
@@ -17,6 +19,24 @@ defmodule ExauthWeb.AuthController do
 
       _ ->
         conn |> render("error.json", %{error: "Internal Server Error"})
+    end
+  end
+
+  def login(conn, %{"username" => username, "password" => password}) do
+    with %User{} = user <- Accounts.get_user_by_username(username),
+         true <- Pbkdf2.verify_pass(password, user.password) do
+      signer =
+        Joken.Signer.create(
+          "HS256",
+          "b6sMoh+DWiHeGTGTu5c87f3+zj7olyjZ1tsZ8jvxYEtGDq4vFTsy5cxH8VFZzj6J"
+        )
+
+      extra_claims = %{user_id: user.id}
+      {:ok, token, _claims} = JWTToken.generate_and_sign(extra_claims, signer)
+
+      conn |> render("login.json", %{success: true, message: "Login Successful", token: token})
+    else
+      _ -> conn |> render("error.json", %{error: "Invalid Credentials"})
     end
   end
 
--- lib/exauth_web/views/auth_view.ex
+++ lib/exauth_web/views/auth_view.ex
@@ -4,6 +4,9 @@ defmodule ExauthWeb.AuthView do
   def render("ack.json", %{success: success, message: message}),
     do: %{success: success, message: message}
 
+  def render("login.json", %{success: success, token: token, message: message}),
+    do: %{success: success, token: token, message: message}
+
   def render("errors.json", %{errors: errors}), do: %{success: false, errors: errors}
   def render("error.json", %{error: error}), do: %{success: false, error: error}
 end
