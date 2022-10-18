8d02cdd7fcca2065537dda0ae99e2493e758298a
twozeronine <amdusias209@gmail.com>
2022-10-18 23:32:00 +0900

    feat : register api

--- lib/exauth_web/controllers/auth_controller.ex
+++ lib/exauth_web/controllers/auth_controller.ex
@@ -1,8 +1,39 @@
 defmodule ExauthWeb.AuthController do
   use ExauthWeb, :controller
+  alias Exauth.Accounts
 
   def ping(conn, _params) do
     conn
     |> render("ack.json", %{success: true, message: "Pong"})
+  end
+
+  def register(conn, params) do
+    case Accounts.create_user(params) do
+      {:ok, _user} ->
+        conn |> render("ack.json", %{success: true, message: "Reistration successful"})
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn |> render("errors.json", %{errors: format_changeset_errors(changeset)})
+
+      _ ->
+        conn |> render("error.json", %{error: "Internal Server Error"})
+    end
+  end
+
+  defp format_changeset_errors(%Ecto.Changeset{} = changeset) do
+    errors =
+      Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+        Enum.reduce(opts, msg, fn {key, value}, acc ->
+          String.replace(acc, "%{#{key}}", to_string(value))
+        end)
+      end)
+
+    formatted_errors =
+      Enum.map(errors, fn {key, value} ->
+        formatted_error = "#{key} #{value}"
+        formatted_error
+      end)
+
+    formatted_errors
   end
 end
--- lib/exauth_web/views/auth_view.ex
+++ lib/exauth_web/views/auth_view.ex
@@ -3,4 +3,7 @@ defmodule ExauthWeb.AuthView do
 
   def render("ack.json", %{success: success, message: message}),
     do: %{success: success, message: message}
+
+  def render("errors.json", %{errors: errors}), do: %{success: false, errors: errors}
+  def render("error.json", %{error: error}), do: %{success: false, error: error}
 end
